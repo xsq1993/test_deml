aae0b15842ea79fdf63b7dff0cf6fac44edcf2e4
2686365963@qq.com <2686365963@qq.com>
2022-01-03 18:00:09 +0800

    第一次上传

---
+++ tp/route/app.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+use think\facade\Route;
+
+Route::get('think', function () {
+
+    return 'hello,ThinkPHP6!';
+});
+
+Route::post('hello/:name', 'index/hello');
+
+Route::post('new/:id','News/read');
